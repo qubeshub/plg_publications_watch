faeca3b769f70bd3623621e1980e47033b4017b4
M. Drew LaMar <mdlama@wm.edu>
2021-07-22 11:00:51 -0400

    [refactor] Pub plugins now using Qubeshub override
    (cherry picked from commit 5743a0cf6eb959eebe3aa572ef538307054e4e1b)

--- watch.php
+++ watch.php
@@ -8,10 +8,12 @@
 // No direct access
 defined('_HZEXEC_') or die();
 
+require_once PATH_APP . DS . 'libraries' . DS . 'Qubeshub' . DS . 'Plugin' . DS . 'Plugin.php';
+
 /**
  * Display Watch feature on publication page
  */
-class plgPublicationsWatch extends \Hubzero\Plugin\Plugin
+class plgPublicationsWatch extends \Qubeshub\Plugin\Plugin
 {
 	/**
 	 * Affects constructor behavior. If true, language files will be loaded automatically.
